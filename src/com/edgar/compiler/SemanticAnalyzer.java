82a447bffcbc95b78abdde5d9a6b829ef8d106dc
WeroVlz <edgarvlzm26@gmail.com>
2024-05-28 23:45:55 -0600

    SemanticAnalyzer v2.0: Type matching in declarations

--- src/com/edgar/compiler/SemanticAnalyzer.java
+++ src/com/edgar/compiler/SemanticAnalyzer.java
@@ -1,20 +1,15 @@
 package com.edgar.compiler;
 
 import java.util.Hashtable;
+import java.util.Stack;
 import java.util.Vector;
 
 public class SemanticAnalyzer {
 
+    private static GUI gui;
     private static final Hashtable<String, Vector<SymbolTableItem>> symbolTable = new Hashtable<>();
+    private static final Stack<String> variableStack = new Stack<>();
 
-    private static final int INT = 0;
-    private static final int FLOAT = 1;
-    private static final int DOUBLE = 2;
-    private static final int BOOLEAN = 3;
-    private static final int CHAR = 4;
-    private static final int STRING = 5;
-    private static final int VOID = 6;
-    private static final int ERR = 7;
     private static final int OP_AND_OR = 0;
     private static final int OP_NOT = 1;
     private static final int OP_EQ_NOTEQ = 2;
@@ -22,6 +17,18 @@ public class SemanticAnalyzer {
     private static final int OP_PLUS = 4;
     private static final int OP_MIN_DIV_MUL = 5;
     private static final int OP_NEG = 6;
+    private static final int OP_ASSIGN = 7;
+
+    private static final Hashtable<String, Integer> VARIABLE_TYPES = new Hashtable<>(){{
+        put("int",0);
+        put("float",1);
+        put("double",2);
+        put("boolean", 3);
+        put("char", 4);
+        put("string",5);
+        put("void",6);
+        put("error",7);
+    }};
 
     private static final String[][][] OPERATORS_CUBE = {
             {
@@ -73,14 +80,38 @@ public class SemanticAnalyzer {
                     {"error", "error", "error", "error", "error", "error", "error","error"}
             }, {
                     {"int", "float", "double", "error", "error", "error", "error","error"},
+            }, {
+                    {"ok", "error", "error", "error", "error", "error", "error","error"},
+                    {"ok", "ok", "error", "error", "error", "error", "error","error"},
+                    {"ok", "ok", "ok", "error", "error", "error", "error","error"},
+                    {"error", "error", "error", "ok", "error", "error", "error","error"},
+                    {"error", "error", "error", "error", "ok", "error", "error","error"},
+                    {"error", "error", "error", "error", "error", "ok", "error","error"},
+                    {"error", "error", "error", "error", "error", "error", "ok","error"},
+                    {"error", "error", "error", "error", "error", "error", "error","error"}
             }
     };
+
+    public static void initializeGui(GUI g){
+        gui = g;
+    }
 
     public static Hashtable<String, Vector<SymbolTableItem>> getSymbolTable(){ return symbolTable; }
 
     public static void clearTable(){ symbolTable.clear(); }
 
-    public static void checkVariable(String id, SymbolTableItem content, int line, GUI gui){
+    public static void pushStack(String type){
+        variableStack.add(type);
+    }
+
+    public static String popStack(){
+        if(!variableStack.isEmpty())
+            return variableStack.pop();
+        else
+            return "";
+    }
+
+    public static void addVariable(String id, SymbolTableItem content, int line){
         boolean isSameScope = false;
         Vector<SymbolTableItem> v = new Vector<>();
         if(!symbolTable.containsKey(id)){
@@ -99,16 +130,56 @@ public class SemanticAnalyzer {
             if (!isSameScope)
                 symbolTable.get(id).add(new SymbolTableItem(content.getType(),content.getScope(),content.getValue()));
             else
-                errorHandler(gui, id, line, 1);
+                errorHandler(id, line, 1);
         }
     }
 
-    public static void errorHandler(GUI gui, String id, int line, int err){
+    public static void assignVariable(String id,int line){
+        if(!symbolTable.containsKey(id)){
+            errorHandler(id,line,2);
+        }
+    };
+
+    public static String getTypeById(String id, int line){
+        Vector<SymbolTableItem> idDeclarations = symbolTable.get(id);
+        if (idDeclarations == null){
+            errorHandler(id, line, 3);
+            return "error";
+        }
+        return idDeclarations.firstElement().getType();
+    }
+
+    public static String calculateOperatorCube(String operator, String variable1){
+      if (operator.equals("-")){
+          return OPERATORS_CUBE[OP_NEG][0][VARIABLE_TYPES.get(variable1)];
+      }else if (operator.equals("!")){
+          return OPERATORS_CUBE[OP_NOT][0][VARIABLE_TYPES.get(variable1)];
+      }else{
+          return OPERATORS_CUBE[2][0][7];
+      }
+    };
+
+    public static String calculateOperatorCube(String operator, String variable1, String variable2){
+        int operatorCube = switch (operator) {
+            case "&", "&&", "|", "||" -> OP_AND_OR;
+            case "==", "!=" -> OP_EQ_NOTEQ;
+            case "<", ">", "<=", ">=" -> OP_MORE_LESS_EQ;
+            case "+" -> OP_PLUS;
+            case "-", "*", "/" -> OP_MIN_DIV_MUL;
+            case "=" -> OP_ASSIGN;
+            default -> 0;
+        };
+
+        return OPERATORS_CUBE[operatorCube][VARIABLE_TYPES.get(variable1)][VARIABLE_TYPES.get(variable2)];
+    }
+
+    public static void errorHandler(String id, int line, int err){
 
         switch (err) {
             case 1 -> gui.writeConsoleLine("Line " + line + ": variable '" + id + "' is already defined");
-            case 2 -> gui.writeConsoleLine("Line " + line + ": variable '" + id + "' is not found");
-
+            case 2 -> gui.writeConsoleLine("Line " + line + ": variable '" + id + "' was never declared");
+            case 3 -> gui.writeConsoleLine("Line " + line + ": variable '" + id + "' was not found");
+            case 4 -> gui.writeConsoleLine("Line " + line + ": incompatible types: type mismatch");
         }
     }
 }
